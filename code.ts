30ecf9e47df1eaec9b6310177ca33f576f5ca88c
Miguel Solorio <miguel.solorio07@gmail.com>
2024-08-28 13:22:04 -0700

    Add theme selector

--- code.ts
+++ code.ts
@@ -24,7 +24,13 @@ checkSelection();
 
 figma.on('selectionchange', checkSelection);
 
-figma.ui.onmessage = (msg: { type: string; width?: number; height?: number }) => {
+figma.ui.onmessage = (msg: { 
+  type: string; 
+  width?: number; 
+  height?: number;
+  colorData?: Array<{ text: string, color: string }>;
+  backgroundColor?: string;
+}) => {
   if (msg.type === 'init') {
     checkSelection();
   } else if (msg.type === 'resize' && msg.width !== undefined && msg.height !== undefined) {
@@ -38,6 +44,28 @@ figma.ui.onmessage = (msg: { type: string; width?: number; height?: number }) =>
     }
     if (newHeight !== currentHeight) {
       figma.ui.resize(currentWidth, newHeight);
+    }
+  } else if (msg.type === 'applyDetailedColors' && msg.colorData && msg.backgroundColor) {
+    const selection = figma.currentPage.selection;
+    if (selection.length === 1 && selection[0].type === 'TEXT') {
+      const textNode = selection[0] as TextNode;
+      
+      // Apply background color
+      const bgColor = figma.util.rgb(msg.backgroundColor);
+      textNode.fills = [{ type: 'SOLID', color: bgColor }];
+      
+      // Apply text colors
+      let currentIndex = 0;
+      msg.colorData.forEach(({ text, color }) => {
+        const endIndex = currentIndex + text.length;
+        const textColor = figma.util.rgb(color);
+        textNode.setRangeFills(currentIndex, endIndex, [{ type: 'SOLID', color: textColor }]);
+        currentIndex = endIndex;
+      });
+      
+      figma.notify('Colors applied successfully!');
+    } else {
+      figma.notify('Please select a single text layer');
     }
   }
 };
