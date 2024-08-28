59416a5012167e84edaa9d0ee1b768156eac21a0
Miguel Solorio <miguel.solorio07@gmail.com>
2024-08-28 10:49:51 -0700

    Init

--- code.ts
+++ code.ts
@@ -1,5 +1,4 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
+// This plugin will highlight the syntax of selected text in Figma
 
 // This file holds the main code for plugins. Code in this file has access to
 // the *figma document* via the figma global object.
@@ -7,28 +6,23 @@
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__html__, { width: 300, height: 400 });
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage =  (msg: {type: string, count: number}) => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+function sendSelectedTextToUI() {
+  const selection = figma.currentPage.selection;
+  if (selection.length === 1 && selection[0].type === 'TEXT') {
+    const textNode = selection[0] as TextNode;
+    const code = textNode.characters;
+    figma.ui.postMessage({ type: 'code', content: code });
+  } else {
+    figma.ui.postMessage({ type: 'no-selection' });
   }
+}
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+figma.on('selectionchange', sendSelectedTextToUI);
+
+figma.ui.onmessage = (msg: { type: string }) => {
+  if (msg.type === 'init') {
+    sendSelectedTextToUI();
+  }
 };
